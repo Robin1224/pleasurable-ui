9155161570dc74ebd901d84313aff6e950ab59cd
Mossati <xmossati9786@gmail.com>
2024-05-16 11:28:19 +0200

    navbar

--- server.js
+++ server.js
@@ -1,1 +1,42 @@
-console.log('Hier komt jullie server')
+// Importeer het npm pakket express uit de node_modules map
+import express from 'express'
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from './helpers/fetch-json.js'
+
+// Haal data op uit de FDND API, ga pas verder als de data gedownload is
+const apiUrl = 'https://fdnd-agency.directus.app/items/'
+const f_houses = apiUrl + 'f_houses'
+const f_list = apiUrl + 'f_list'
+const f_files = apiUrl + 'f_houses_files'
+const f_feedback = apiUrl + 'f_feedback'
+
+// Maak een nieuwe express app aan
+const app = express()
+
+// Stel ejs in als template engine
+app.set('view engine', 'ejs')
+// Stel de map met ejs templates in
+app.set('views', './views')
+
+// Gebruik de map 'public' voor statische resources
+app.use(express.static('public'))
+
+//Verwerken van url-gecodeerde data in POST-verzoeken
+app.use(express.urlencoded({extended: true}))
+
+// GET route voor de index pagina
+app.get('/', function (request, response) {
+    fetchJson(f_houses).then((housesData) => {
+        response.render('index', {houses: housesData.data})
+    })
+})
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8000)
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+    console.log(`Application started on http://localhost:${app.get('port')}`)
+})
