18527d8ec5e70baa4b2b1a8d0a5f1a0241cfbf9f
Remy Duivesteijn <remyd03@icloud.com>
2024-05-16 15:06:13 +0200

    v1.0 | Setup complete

--- server.js
+++ server.js
@@ -1,1 +1,54 @@
-console.log('Hier komt jullie server')
+// Importeer het npm pakket express uit de node_modules map
+import express from "express";
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from "./helpers/fetch-json.js";
+
+// Haal data op uit de Funda API, ga pas verder als de data gedownload is
+const apiUrl = "https://fdnd-agency.directus.app/items/f_houses";
+
+// Maak een nieuwe express app aan
+const app = express();
+
+// Stel ejs in als template engine
+app.set("view engine", "ejs");
+
+// Stel de map met ejs templates in
+app.set("views", "./views");
+
+// Gebruik de map 'public' voor statische resources
+app.use(express.static("public"));
+
+// Zorg dat werken met request data makkelijker wordt
+app.use(express.urlencoded({ extended: true }));
+
+// Maak een GET route voor de index
+app.get("/", function (request, response) {
+    //Fetch data uit de API en geef parameter door
+    fetchJson(apiUrl).then((housesData) => {
+        // Render index.ejs uit de views map en geef uit FDND API opgehaalde data mee
+        response.render("index", { houses: housesData.data });
+    });
+});
+
+// Maak een GET route voor de index
+app.get("/detail/:id", function (request, response) {
+    fetchJson(apiUrl + "/" + request.params.id).then((housesData) => {
+        response.render("detail", { house: housesData.data });
+    });
+});
+
+// Maak een POST route voor de index
+app.post("/", function (request, response) {
+    // Er is nog geen afhandeling van POST, redirect naar GET op /
+    response.redirect(303, "/");
+});
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set("port", process.env.PORT || 8000);
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get("port"), function () {
+    // Toon een bericht in de console en geef het poortnummer door
+    console.log(`Application started on http://localhost:${app.get("port")}`);
+});
